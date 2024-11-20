d058931e17e4ab07dcc17383b14bf65fba35573e
palakm17 <mishrapalak4477@gmail.com>
2024-11-20 14:23:08 +0530

    Create variables.c

---
+++ program/variables.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main(){
+    /// Declare Variables
+    int i;
+    char c;
+    float f;
+    double d;
+    long int li;
+    short int si;
+
+    /// Initialize Variable
+    i = 3345;
+    c = 'c';
+    f = 2323.643;
+    d = 34543.356563;
+    li = 46576;
+    si = 254;
+
+
+
+    /// Show Variable Values
+    printf("%i\n",i);
+    printf("%c\n",c);
+    printf("%f\n",f);
+    printf("%lf\n",d);
+    printf("%ld\n",li);
+    printf("%hi\n",si);
+
+
+}
